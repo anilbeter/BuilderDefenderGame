d2464b906d495528072f394aa8e03db201a56d60
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-19 22:07:57 +0300

    setup BuildingManager script for spawn WoodHarvester object when left click

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class BuildingManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  // Reference for WoodHarvester prefab
+  [SerializeField] private Transform pfWoodHarvester;
+  private Camera mainCamera;
 
-    // Update is called once per frame
-    void Update()
+  private void Start()
+  {
+    mainCamera = Camera.main;
+  }
+
+
+  private void Update()
+  {
+    // Spawn WoodHarvester object at current mouse cursor position when clicked left mouse button, with no rotation(Quaternion.identity)
+    if (Input.GetMouseButtonDown(0))
     {
-        
+      Instantiate(pfWoodHarvester, GetMouseWorldPosition(), Quaternion.identity);
     }
+  }
+
+  // Get mouse position 
+  private Vector3 GetMouseWorldPosition()
+  {
+    Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+    mouseWorldPosition.z = 0;
+    return mouseWorldPosition;
+  }
 }
