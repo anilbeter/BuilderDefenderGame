bd86daaaf234ce8045c76b09c228d3305b5e526b
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-20 08:46:15 +0300

    setup BuildingTypeListSO script

--- Assets/Scripts/BuildingTypeListSO.cs
+++ Assets/Scripts/BuildingTypeListSO.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BuildingTypeListSO : MonoBehaviour
+[CreateAssetMenu(menuName = "ScriptableObjects/BuildingTypeList")]
+public class BuildingTypeListSO : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  public List<BuildingTypeSO> buildingTypeList;
 }
